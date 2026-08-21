4f749d1a418e6f5216cd40b438d9b66afde930f7
timling234 <149541718+timling234@users.noreply.github.com>
2026-08-21 02:02:26 -0400

    Add ecosystem.config.cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: "dmhelper-backend",
+      script: "server/index.js",
+      cwd: __dirname,
+      autorestart: true,
+      watch: false
+    },
+    {
+      name: "dmhelper-frontend",
+      script: "npm",
+      args: ["run", "dev", "--", "--host", "0.0.0.0", "--port", "5174"],
+      cwd: __dirname + "/web",
+      autorestart: true,
+      watch: false
+    },
+    {
+      name: "dmhelper-tunnel",
+      script: "cloudflared",
+      args: "tunnel run dmhelper",
+      cwd: __dirname,
+      autorestart: true,
+      watch: false
+    }
+  ]
+};
+
+
