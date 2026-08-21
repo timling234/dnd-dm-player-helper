b11d292316127f9e549549e561b77530f6459540
timling234 <149541718+timling234@users.noreply.github.com>
2026-08-21 02:03:10 -0400

    Add web/src/main.tsx

---
+++ web/src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dm" element={<App />} />
+        <Route path="/player" element={<App />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+);
+
